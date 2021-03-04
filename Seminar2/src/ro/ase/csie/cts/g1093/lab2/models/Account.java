08d34f6cefc35bbe882b339ee6540e9e375a377c
BogdanHriscu1 <hriscubogdan18@stud.ase.ro>
2021-03-04 15:33:43 +0200

    Seminar 2 final

--- Seminar2/src/ro/ase/csie/cts/g1093/lab2/models/Account.java
+++ Seminar2/src/ro/ase/csie/cts/g1093/lab2/models/Account.java
@@ -1,9 +1,12 @@
 package ro.ase.csie.cts.g1093.lab2.models;
 
+import ro.ase.csie.cts.g1093.lab2.exceptions.IllegalTransferException;
+import ro.ase.csie.cts.g1093.lab2.exceptions.InsufficientFundsException;
+
 public abstract class Account {
 
 	public abstract double getBalance();
 	public abstract void deposit(Double amount);
-	public abstract void withdraw(Double amount);
-	public abstract void Transfer(Account destination, Double amount);
+	public abstract void withdraw(Double amount) throws InsufficientFundsException;
+	public abstract void Transfer(Account destination, Double amount) throws InsufficientFundsException, IllegalTransferException;
 }
