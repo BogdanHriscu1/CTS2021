a7c0ce7a0f8bebd69c742e47e8baa7491289791d
BogdanHriscu1 <hriscubogdan18@stud.ase.ro>
2021-03-18 14:21:21 +0200

    Seminar4 before break

---
+++ Seminar3/src/ro/ase/csie/cts/lab3/stage3/ProductType.java
@@ -0,0 +1,21 @@
+package ro.ase.csie.cts.lab3.stage3;
+
+public enum ProductType {
+	
+	NEW(0), DISCOUNT(0.1f), LIMITED_STOCK(0.25f), LEGACY(0.35f);
+	
+	private final float discount;
+	
+	private ProductType(float discount) {
+		this.discount = discount;
+	}
+	
+//	private ProductType() {
+//		this.discount = 0;
+//	}
+	// we can declare this constructor above
+	
+	public float getDiscount() {
+		return this.discount;
+	}
+}
