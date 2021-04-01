3d8da757540b612b6cb3b33ed6640e135105daed
BogdanHriscu1 <hriscubogdan18@stud.ase.ro>
2021-04-01 15:20:13 +0300

    Finish Builder - After Break

--- Seminar6 - Prototype Builder/src/ro/ase/acs/cts/dp/builder/TestBuilder.java
+++ Seminar6 - Prototype Builder/src/ro/ase/acs/cts/dp/builder/TestBuilder.java
@@ -4,10 +4,19 @@ public class TestBuilder {
 
 	public static void main(String[] args) {
 		
-		SuperHero superHero = new SuperHero();
-		SuperHero superHero2 = new SuperHero("Superman", 100, false, false, new Weapon(), 
-				null, new Flying(), null);	
+//		SuperHero superHero = new SuperHero();
+//		SuperHero superHero2 = new SuperHero("Superman", 100, false, false, new Weapon(), 
+//				null, new Flying(), null);	
 
+		SuperHero superman = 
+				new SuperHero.SuperHeroBuilder("Superman", 100).build();
+		
+		SuperHero joker =
+				new SuperHero.SuperHeroBuilder("Joker", 200)
+				.isVillain()
+				.isWounded()
+				.setRightWeapon(new Weapon())
+				.build();
 	}
 
 }
