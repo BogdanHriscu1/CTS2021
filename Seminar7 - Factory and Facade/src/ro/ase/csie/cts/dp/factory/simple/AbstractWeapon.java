e411efdf480612a34665fb08bdbe7407487a37ac
BogdanHriscu1 <hriscubogdan18@stud.ase.ro>
2021-04-08 14:33:19 +0300

    Commit before the break - Simple Factory

---
+++ Seminar7 - Factory and Facade/src/ro/ase/csie/cts/dp/factory/simple/AbstractWeapon.java
@@ -0,0 +1,5 @@
+package ro.ase.csie.cts.dp.factory.simple;
+
+public abstract class AbstractWeapon {
+
+}
---
+++ Seminar7 - Factory and Facade/src/ro/ase/csie/cts/dp/factory/simple/TestFactory.java
@@ -0,0 +1,10 @@
+package ro.ase.csie.cts.dp.factory.simple;
+
+public class TestFactory {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
